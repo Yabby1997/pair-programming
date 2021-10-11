46544a011b7f16ed4d0d82977098b589d50b8b79
Kinaan-Oh <blackshooter@hanmail.net>
2021-10-11 16:55:43 +0900

    error

--- pair-programming/ViewController.swift
+++ pair-programming/ViewController.swift
@@ -6,9 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    // MARK: - Subviews
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .orange
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = .red
+        return contentView
+    }()
+    
+    private lazy var someView: UIView = {
+        let someView = UIView()
+        someView.backgroundColor = .brown
+        return someView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.topMargin.equalTo(view.snp.topMargin)
+            make.left.equalTo(view.snp.left)
+            make.right.equalTo(view.snp.right)
+            make.bottomMargin.equalTo(view.snp.bottomMargin)
+        }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.top.bottom.equalToSuperview()
+        }
+        
+        someView.addSubview(contentView)
+        someView.snp.makeConstraints { make in
+            make.width.equalTo(contentView.snp.width)
+            make.height.equalTo(1500)
+            make.bottom.equalTo(contentView.snp.bottom)
+        }
     }
 }
