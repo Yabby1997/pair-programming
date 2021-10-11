3217e8bf3d54ffedc27983b3539c59361a2319fb
yabby <yabby1997@gmail.com>
2021-10-11 18:19:00 +0900

    SceneDelegate에 print문 추가

--- pair-programming/SceneDelegate.swift
+++ pair-programming/SceneDelegate.swift
@@ -13,6 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
+        print("say something")
         
         window = UIWindow(windowScene: scene)
         let viewController = ViewController()
@@ -47,7 +48,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
